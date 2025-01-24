a605ceb0bc1c6a27e5a507f48557558326f059fd
rebornrival <renee@devivo.com>
2025-01-23 21:36:49 -0700

    FInished dialogue trigger node YAYYYYYY

--- DialogueTrigger.cs
+++ DialogueTrigger.cs
@@ -8,6 +8,8 @@ public partial class DialogueTrigger : Node2D
 	public Resource test;
 	[Export]
 	public bool startWithScene = false;
+	[Export]
+	public PackedScene player;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -20,5 +22,12 @@ public partial class DialogueTrigger : Node2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+
+	private void _on_area_2d_body_entered(Node2D body) {
+		if(body.Name == "Player" && !startWithScene){
+			DialogueManager.ShowExampleDialogueBalloon(test, "start");
+		}
+
 	}
 }
