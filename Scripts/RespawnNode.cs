45dead961faf3b5ce8c3d2cb3b849d11bf7cf763
rebornrival <renee@devivo.com>
2025-01-25 17:38:13 -0700

    respawn node (hopefully)

---
+++ Scripts/RespawnNode.cs
@@ -0,0 +1,25 @@
+using Godot;
+using System;
+
+public partial class RespawnNode : Node2D
+{
+	[Export]
+	public Node2D respawnPosition;
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+	
+	private void _on_area_2d_body_entered(Node2D body) {
+		if(body is Player){
+			respawnPosition.Transform = this.Transform;
+		}
+		GD.Print("haha");
+}
+}
