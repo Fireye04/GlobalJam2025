d7125abd53b3da43eb0fc770dee265d3044c9234
Fireye <codekai16@gmail.com>
2025-01-25 22:38:28 -0700

    quicktime updates

--- Scenes/Quicktime.cs
+++ Scenes/Quicktime.cs
@@ -7,36 +7,91 @@ public partial class Quicktime : CanvasLayer
     [Export]
     public Timer timeyBoi;
 
-    public int spamCount = 0;
+    [Export]
+    public ProgressBar pBar;
 
-    public int spamGoal = 100;
+    [Export]
+    public Label prompt;
+
+    [Export]
+    public float drainSpeed = 10;
+
+    private int totalTime = 20;
+    private Random rnd = new Random();
+
+    private int currentKey = 0;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+        StartQT(20);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+        if (totalTime != 0) {
+            pBar.Value -= drainSpeed * delta;
+        }
+
+        if (pBar.Value == 0) {
+            prompt.Text = "FAIL";
+            totalTime = 0;
+            timeyBoi.Stop();
+            return;
+        }
+
         if (Input.IsActionJustPressed("qt1")) {
-            
+           if (currentKey == 0){
+                pBar.Value += 1;
+           } 
         }
 
         if (Input.IsActionJustPressed("qt2")) {
-
+           if (currentKey == 1){
+                pBar.Value += 1;
+           } 
         } 
 
         if (Input.IsActionJustPressed("qt3")) {
-
+           if (currentKey == 2){
+                pBar.Value += 1;
+           } 
         }
 
 	}
 
-    public void StartQT(int spamGoal) {
-        spamCount = 0; 
+    public void StartQT(int seconds) {
+        totalTime = seconds;
+        startSection();
+
     }
 
+    private void startSection() {
+        if (totalTime == 0) {
+            return;
+        }
+        int length = rnd.Next(1, ((int)(totalTime/2))+1);
+        timeyBoi.Start(length);
+        totalTime -= length;
+        int key = rnd.Next(3);
+        if (key == 0) {
+            prompt.Text = "SPAM Z";
+        } else if (key == 1) {
+            prompt.Text = "SPAM X";
+        } else {
+            prompt.Text = "SPAM C";
+        }
+        currentKey = key;
+
+    }
+
+    private void _on_timer_timeout(){
+        startSection();
+        if (totalTime == 0) {
+            prompt.Text = "SUCCESS";
+        }
+    }
     
 
 }
