ac5a79476fd36dd2596101eb979c56e088d40144
Fireye <codekai16@gmail.com>
2025-01-25 23:05:11 -0700

    bubbles-fixes

--- Scripts/Tools/Bubbles.cs
+++ Scripts/Tools/Bubbles.cs
@@ -3,9 +3,14 @@ using System;
 
 public partial class Bubbles : Node2D
 {
+
+    [Export]
+    public AnimatedSprite2D spriteAnim;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+        spriteAnim.Play("default");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
