88620dd2fd9c4043970514c2905e87dbb5167cdb
Fireye <codekai16@gmail.com>
2025-01-25 19:37:17 -0700

    Save qt

--- Scenes/Quicktime.cs
+++ Scenes/Quicktime.cs
@@ -3,6 +3,11 @@ using System;
 
 public partial class Quicktime : CanvasLayer
 {
+
+    [Export]
+    public Timer timeyBoi;
+
+    private int spamCount = 0;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -11,5 +16,24 @@ public partial class Quicktime : CanvasLayer
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+        if (Input.IsActionJustPressed("qt1")) {
+            
+        }
+
+        if (Input.IsActionJustPressed("qt2")) {
+
+        } 
+
+        if (Input.IsActionJustPressed("qt3")) {
+
+        }
+
 	}
+
+    public void StartQT() {
+        spamCount = 0; 
+    }
+
+    
+
 }
